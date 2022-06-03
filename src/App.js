9bed807dc870ccc45030a961508cac1768c18aeb
Kerim <kerimberdi.agayev@edusoftech.eu>
2022-06-03 14:53:38 +0300

    added coming soon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ComingSoon from 'react-coming-soon'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <ComingSoon title="Kerimberdi Agayev" subtitle="I am building my portfolio" bgColor="#fff" textColor="#212121" date="Thu Jun 14 2022 00:00:00 GMT+0300 (Eastern European Summer Time)" illustration="git" />
+    
   );
 }
 
